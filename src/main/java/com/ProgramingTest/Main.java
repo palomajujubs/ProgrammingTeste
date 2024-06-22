6fdd79c85e5d0fcf2a2a448e103bd8c3ebd68fce
unknown <paloma.sousa.santos@hotmail.com>
2024-06-22 11:00:55 -0300

    finalizado

--- src/main/java/com/ProgramingTest/Main.java
+++ src/main/java/com/ProgramingTest/Main.java
@@ -2,76 +2,143 @@ package com.ProgramingTest;
 
 import Domain.models.Funcionario;
 
+
 import java.math.BigDecimal;
+
+import java.math.RoundingMode;
 import java.time.LocalDate;
-import java.util.ArrayList;
 
+import java.util.*;
 
-import static Domain.models.Pessoa.getFormatter;
 
 
 
 public class Main {
+
+
+
     public static void main(String[] args) {
-
-
 
 
         ArrayList<Funcionario> funcionarios = new ArrayList<Funcionario>();
 
-        funcionarios.add(new Funcionario("Maria",
-                LocalDate.of(2000, 10, 18),
-                "Operador", new BigDecimal("2009.44")));
+        funcionarios.add(new Funcionario("Maria", LocalDate.of(2000, 10, 18), "Operador", new BigDecimal("2009.44")));
 
-        funcionarios.add(new Funcionario("João",
-                LocalDate.of(1990, 6, 12),
-                "Operador", new BigDecimal("2284.38")));
+        funcionarios.add(new Funcionario("João", LocalDate.of(1990, 6, 12), "Operador", new BigDecimal("2284.38")));
 
-        funcionarios.add(new Funcionario("Caio",
-                LocalDate.of(1961, 5, 2),
-                "Coordenador", new BigDecimal("9834.14")));
+        funcionarios.add(new Funcionario("Caio", LocalDate.of(1961, 5, 2), "Coordenador", new BigDecimal("9834.14")));
 
-        funcionarios.add(new Funcionario("Miguel",
-                LocalDate.of(1988, 10, 14),
-                "Diretor", new BigDecimal("2009.44")));
+        funcionarios.add(new Funcionario("Miguel", LocalDate.of(1988, 10, 14), "Diretor", new BigDecimal("2009.44")));
 
-        funcionarios.add(new Funcionario("Alice",
-                LocalDate.of(1995, 1, 5),
-                "Recepcionista", new BigDecimal("2243.68")));
+        funcionarios.add(new Funcionario("Alice", LocalDate.of(1995, 1, 5), "Recepcionista", new BigDecimal("2243.68")));
 
-        funcionarios.add(new Funcionario("Heitor",
-                LocalDate.of(1999, 11, 19),
-                "Operador", new BigDecimal("1582.72")));
+        funcionarios.add(new Funcionario("Heitor", LocalDate.of(1999, 11, 19), "Operador", new BigDecimal("1582.72")));
 
-        funcionarios.add(new Funcionario("Arthur",
-                LocalDate.of(1993, 3, 31),
-                "Contador", new BigDecimal("4071.84")));
+        funcionarios.add(new Funcionario("Arthur", LocalDate.of(1993, 3, 31), "Contador", new BigDecimal("4071.84")));
 
-        funcionarios.add(new Funcionario("Laura",
-                LocalDate.of(1994, 7, 8),
-                "Gerente", new BigDecimal("3017.45")));
+        funcionarios.add(new Funcionario("Laura", LocalDate.of(1994, 7, 8), "Gerente", new BigDecimal("3017.45")));
 
-        funcionarios.add(new Funcionario("Heloísa",
-                LocalDate.of(2003, 6, 24),
-                "Eletricista", new BigDecimal("1606.85")));
+        funcionarios.add(new Funcionario("Heloísa", LocalDate.of(2003, 6, 24), "Eletricista", new BigDecimal("1606.85")));
 
-        funcionarios.add(new Funcionario("Helena",
-                LocalDate.of(1996, 9, 2),
-                "Gerente", new BigDecimal("2799.93")));
+        funcionarios.add(new Funcionario("Helena", LocalDate.of(1996, 9, 2), "Gerente", new BigDecimal("2799.93")));
 
         funcionarios.removeIf(funcionario -> funcionario.getNome().equals("João"));
 
-        for (int i = 0; i < funcionarios.size(); ++i){
-            Funcionario funcionario = funcionarios.get(i);
+        imprimirTitulo("Funcionários");
+        listaFuncionarios(funcionarios);
 
-             String nome = funcionario.getNome();
-             String dataNascimento = funcionario.getDataNascimento().format(getFormatter());
-             String funcao = funcionario.getFuncao();
-             BigDecimal salario = funcionario.getSalario();
-             BigDecimal salariaCal;
-            System.out.printf("Nome: %s, Data de nascimento: %s, Fnção : %s, Salario: %s%n",nome,dataNascimento, funcao, salario );
+        for (Funcionario funcionario : funcionarios) {
+
+            BigDecimal salario = funcionario.getSalario();
+            BigDecimal aumento = salario.multiply(new BigDecimal("0.10"));
+            BigDecimal salarioAtualizado = salario.add(aumento);
+
+            funcionario.setSalario(salarioAtualizado);
+        }
+
+        imprimirTitulo("Funcionários com aumento salarial");
+        listaFuncionarios(funcionarios);
+
+        Map<String, List<Funcionario>> conjuntoFuncionarios = new HashMap<>();
+
+        for (Funcionario funcionario : funcionarios) {
+            if (!conjuntoFuncionarios.containsKey(funcionario.getFuncao()))
+                conjuntoFuncionarios.put(funcionario.getFuncao(), new ArrayList<>());
+
+            conjuntoFuncionarios.get(funcionario.getFuncao()).add(funcionario);
+        }
+
+        imprimirTitulo("Imprimindo funcionários por função");
+
+        for (var funcao : conjuntoFuncionarios.keySet()) {
+            List<Funcionario> listaFuncionarios = conjuntoFuncionarios.get(funcao);
+
+            imprimirTitulo(funcao);
+            listaFuncionarios(listaFuncionarios);
+        }
+        imprimirTitulo("Imprimindo funcionários que fazem aniversario no mês 10 e 12");
+
+        List<Funcionario> funcionariosPorMes = funcionarios
+                .stream()
+                .filter(f -> f.getDataNascimento().getMonthValue() == 10 || f.getDataNascimento().getMonthValue() == 12)
+                .toList();
+
+        listaFuncionarios(funcionariosPorMes);
+
+        imprimirTitulo("Imprimindo funcionário com maior idade");
+
+        Funcionario funcionarioMaisVelho = funcionarios.stream().reduce((f1, f2) -> {
+            if (f2.getIdade() > f1.getIdade()) return f2;
+            else return f1;
+        }).orElseThrow();
+
+        funcionarioMaisVelho.imprimirNomeEIdade();
+
+        funcionarios.stream().map(f -> f.getNome());
+
+
+
+        imprimirTitulo("Imprimindo funcionários em ordem alfabética");
+
+        Collections.sort(funcionarios, (f1, f2) -> f1.getNome().compareTo(f2.getNome()));
+
+        listaFuncionarios(funcionarios);
+
+        imprimirTitulo("Total de salarios dos funcionarios");
+        BigDecimal totalSalario = BigDecimal.ZERO;
+        for ( Funcionario funcionario : funcionarios){
+            if (funcionario.getSalario()!=null) {
+                totalSalario = totalSalario.add(funcionario.getSalario());
+
+            }
+        }
+        System.out.printf("salario: %.2f\n", totalSalario);
+
+        imprimirTitulo("Quantos salarios minimos ganha cada funcionário");
+        for (Funcionario funcionario: funcionarios){
+
+            funcionario.imprimirNomeESalariosMinimos();
 
         }
+
+
     }
 
+
+
+
+
+    private static void imprimirTitulo(String titulo) {
+        String linha = "*".repeat(titulo.length() + 4);
+        System.out.println(linha);
+        System.out.printf("* %s *\n", titulo);
+        System.out.println(linha);
+        System.out.println();
+    }
+
+    private static void listaFuncionarios(List<Funcionario> lista) {
+        for (Funcionario funcionario : lista) {
+            System.out.println(funcionario);
+        }
+    }
 }
